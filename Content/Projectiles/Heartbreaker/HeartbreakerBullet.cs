263fe9378f27bb130337846da3cce6bc64881afe
Rapoulas <rafaelcesargomes@gmail.com>
2024-06-12 18:02:26 -0300

    Heartbreaker WIP

--- Content/Projectiles/Heartbreaker/HeartbreakerBullet.cs
+++ Content/Projectiles/Heartbreaker/HeartbreakerBullet.cs
@@ -10,9 +10,10 @@ namespace RappleMod.Content.Projectiles.Heartbreaker
 {   
     public class HeartbreakerBullet : ModProjectile
     {
-        public bool isHeartbeat = false;
-        public int heartbeatTimer = 0;
-        public int heartbeatProgress = 1;
+        bool isHeartbeat = false;
+        int heartbeatTimer = 0;
+        int heartbeatProgress = 1;
+        int direction = 1;
         public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.MeteorShot;
         public override void SetDefaults() {
             Projectile.width = 2;
@@ -22,17 +23,12 @@ namespace RappleMod.Content.Projectiles.Heartbreaker
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.tileCollide = true;
             Projectile.usesLocalNPCImmunity = false;
+            Projectile.timeLeft = 600;
         }
 
         public override void AI(){
             float rotation = 0;
             Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
-
-            Projectile.ai[0]++;
-            if (Projectile.ai[0] % 60 == 0){
-                    Projectile.ai[0] = 0;
-                    isHeartbeat = true;
-            }
 
             if (isHeartbeat){
                 if (heartbeatTimer == 0) heartbeatProgress = 1;
@@ -41,20 +37,57 @@ namespace RappleMod.Content.Projectiles.Heartbreaker
                 else if (heartbeatTimer == 45) heartbeatProgress = 4;
                 else if (heartbeatTimer == 60) heartbeatProgress = 5;
                 else if (heartbeatTimer == 75) heartbeatProgress = 6;
+                else if (heartbeatTimer == 90) heartbeatProgress = 7;
 
                 if (heartbeatProgress == 1){
-                    rotation = -70 * Projectile.direction;
+                    rotation = -70 * direction;
                     Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
                     heartbeatProgress = -1;
                 }
                 if (heartbeatProgress == 2){
-                    rotation = 140 * Projectile.direction;
+                    rotation = 140 * direction;
                     Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
                     heartbeatProgress = -1;
                 }
-
+                else if (heartbeatProgress == 3){
+                    rotation = 210 * direction;
+                    Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
+                    heartbeatProgress = -1;
+                }
+                else if (heartbeatProgress == 4){
+                    rotation = 140 * direction;
+                    Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
+                    heartbeatProgress = -1;
+                }
+                else if (heartbeatProgress == 5){
+                    rotation = 210 * direction;
+                    Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
+                    heartbeatProgress = -1;
+                }
+                else if (heartbeatProgress == 6){
+                    rotation = 140 * direction;
+                    Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
+                    heartbeatProgress = -1;
+                }
+                else if (heartbeatProgress == 7){
+                    rotation = -50 * direction;
+                    Projectile.velocity = Projectile.velocity.RotatedBy(MathHelper.ToRadians(rotation));
+                    heartbeatProgress = -1;
+                    isHeartbeat = false;
+                    heartbeatTimer = -1;
+                }
+                
                 heartbeatTimer++;
             }
+            else Projectile.ai[0]++;
+
+            if (Projectile.ai[0] % 60 == 0){
+                    Projectile.ai[0] = 1;
+                    isHeartbeat = true;
+                    direction = Projectile.direction;
+            }
+
+            int bulletTrail = Dust.NewDust(Projectile.Center, 0, 0, DustID.Firework_Yellow, 0f, 0f, 0, Color.Transparent);
         }
 
         public override bool OnTileCollide(Vector2 oldVelocity)
--- obj/Debug/net6.0/RappleMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/RappleMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RappleMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df8be9cf5e04893b9f669efc5c971554115234e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ca808d09a36caa666601262e53d5ebcb2f61c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RappleMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RappleMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
